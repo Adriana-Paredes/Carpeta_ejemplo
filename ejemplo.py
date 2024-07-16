e8b2c80f6d94f269cd8fd0a2628e92512db2bad6
Adriana Paredes <adriana.paredes@estudiantes.uamerica.edu.co>
2024-07-16 10:35:43 -0500

    Grafica nueva

---
+++ ejemplo.py
@@ -0,0 +1,10 @@
+# Números del 1 al 10
+
+cont= 0
+num_list= []
+
+while cont <=10:
+    num_list.append(cont)
+    cont +=1
+
+print(num_list)
---
+++ plt.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace (-5,5,100)
+y= x**3
+plt.plot(x,y, "r--", label="x^3", linewidth=3)
+plt.title("Función en python con numpy y matplotlib")
+plt.legend()
+plt.grid()
+plt.show()
+
