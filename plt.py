2c8963ddc29ca20407c4637dc57dfa354537bede
Adriana Paredes <adriana.paredes@estudiantes.uamerica.edu.co>
2024-07-16 10:52:29 -0500

    Cambios

--- plt.py
+++ plt.py
@@ -3,7 +3,7 @@ import numpy as np
 
 x = np.linspace (-5,5,100)
 y= x**3
-plt.plot(x,y, "g.-", label="$x^3$", linewidth=5)
+plt.plot(x,y, "r--", label="x^3", linewidth=3)
 plt.title("Función en python con numpy y matplotlib")
 plt.legend()
 plt.grid()
